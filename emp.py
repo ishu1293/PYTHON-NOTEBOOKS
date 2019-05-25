7477e965fc503cfbf6b8daac4813c59366be88d6
AISHWARYA SHARMA <42669615+ishu1293@users.noreply.github.com>
2019-05-25 11:31:19 +0530

    Add files via upload

---
+++ emp.py
@@ -0,0 +1,10 @@
+import pickle
+class employee:
+    def __init__(self,eno,ename,esal,eaddr):
+        self.eno=eno
+        self.ename=ename
+        self.esal=esal
+        self.eaddr=eaddr
+    def display(self):
+        print(self.eno,"\t",self.ename,"\t",self.esal,"\t",self.eaddr)
+   
